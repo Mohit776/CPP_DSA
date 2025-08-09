7e7416f9040b77ba46902ef0ad3c6ebd476ac93d
Mohit776 <mohitaggarwal551@gmail.com>
2025-08-09 22:57:04 +0530

    array

---
+++ Array/TrappedWater.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n =  height.size();
+        if (n == 0) return 0;
+        vector<int>lmax(n,0);
+        vector<int>rmax(n,0);
+
+        lmax[0]= height[0];
+        rmax[n-1]= height[n-1];
+
+        //FOR LMAX
+        for (int i = 1; i < n; i++)
+        {
+            lmax[i] = max(lmax[i-1],height[i]);
+        }
+        
+         //FOR RMAX
+        for (int i = n-2; i >= 0; i--)
+        {
+            rmax[i] = max(rmax[i+1],height[i]);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            ans += min(lmax[i],rmax[i]) - height[i];
+        }
+
+        return ans;
+        
+    }
+};
+
+int main()
+{
+
+    vector<int> arr = {4,2,0,3,2,5};
+    Solution s;
+
+    s.trap(arr);
+
+    return 0;
+}
