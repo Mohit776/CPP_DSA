f7a1d50a0808b5bfde18411dddbfbdb6d13c1a63
Mohit776 <mohitaggarwal551@gmail.com>
2025-08-12 23:43:01 +0530

    array

--- Array/Dulicate.cpp
+++ Array/Dulicate.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
 #include <stack>
 #include <vector>
+#include <unordered_map>
 using namespace std;
+
+class Solution
+{
+public:
+    int findDuplicate(vector<int> &nums)
+    {
+        unordered_map<int, int> m;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (m.find(nums[i]) != m.end())
+            {
+                return nums[i];
+            }
+            m[nums[i]] = i;
+        }
+
+        return -1;
+    }
+
+
+//=================================================
+
+};
 
 int main()
 {
-    vector<int> arr = {6,8,0,1,3};
+    vector<int> arr = {6, 8, 0, 1, 3};
 
     return 0;
 }
---
+++ LinkListed/Add2Numsbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> arr = {6,8,0,1,3};
+
+    return 0;
+}
