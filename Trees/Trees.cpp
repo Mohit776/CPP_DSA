d8509c69b318021c5b7ea52eba655b7b0942c04c
Mohit776 <mohitaggarwal551@gmail.com>
2025-08-22 23:34:44 +0530

    trees

---
+++ Trees/Trees.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+Node* BuildTree(vector<int>preorder){
+    idx ++;
+    if(preorder[idx] == -1)return NULL;
+
+    Node* root = new Node(preorder[idx]);
+
+   root->left =  BuildTree(preorder); //left sub tree
+   root -> right = BuildTree(preorder); // Right sub tree
+
+   return root;
+}
+
+
+
+
+int main()
+{
+   
+    vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+
+    Node* root = BuildTree(preorder);
+    cout << root-> data << endl;
+    cout << root-> right -> left -> data ;
+
+
+    return 0;
+}
